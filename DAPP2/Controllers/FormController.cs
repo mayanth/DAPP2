81a30904c85b4d2ef5d2b562327f18682d547298
user <user@DESKTOP-CVSC595>
2022-08-01 17:23:32 +0700

    Add project files.

---
+++ DAPP2/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAPP2.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult House()
+        {
+            return View();
+        }
+        public IActionResult Land()
+        {
+            return View();
+        }
+        public IActionResult Price()
+        {
+            return View();
+        }
+    }
+}
+
